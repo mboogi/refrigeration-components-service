95c6433b4290a475633d7765a431909deb3491b4
MB <bog_milos@yahoo.com>
2022-12-11 20:01:19 +0100

    wip valves

---
+++ src/test/kotlin/refrigeration/components/selector/config/valves/crud/ValveControllerTest.kt
@@ -0,0 +1,12 @@
+package refrigeration.components.selector.config.valves.crud
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ValveControllerTest {
+
+    @Test
+    fun getAll() {
+    }
+}
