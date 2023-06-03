faa62c30f6dab2e909138e28c55f97632ca565c9
MB <bog_milos@protonmail.com>
2023-06-03 09:09:21 +0200

    first working version of docker jar file

---
+++ deployment/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+./gradlew build && java -jar build/libs/components-selector-0.0.1-SNAPSHOT-plain.jar
+sudo docker build --build-arg JAR_FILE=build/libs/components-selector-0.0.1-SNAPSHOT-plain.jar -t bogmilos/be-components-selector:2.0.0 .
+#sudo docker run -p 8080:8080 bogmilos/be-sniffer-service:latest
+sudo docker login
+sudo docker push bogmilos/be-components-selector:2.0.0
